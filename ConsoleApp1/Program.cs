142e0f22d742fd91585b6aa076f5a0324abea049
Артем Сало <artemsalo15@gmail.com>
2025-01-17 21:45:38 +0500

    поменял имя файла

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace Task_02_04
 {
 
     //Пользователь вводит своё д/р произвести расчет является ли пользователь соввершеннолетним на текущаю дату и вывести сообщение об этом
