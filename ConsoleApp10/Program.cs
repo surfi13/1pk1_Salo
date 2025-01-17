b6a05d23bec1d983cf679609cab4e32abd59831e
Admin <Admin@HP255G8>
2025-01-17 20:31:08 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double a = 14;
+        double b = 8;
+        double c = Math.PI / 4;
+
+        double p1 = Math.Pow(a - 1, 1/3);
+        double p2 = Math.Pow(b + p1, 1/4);
+        double p3 = Math.Abs(a -b) * (Math.Pow(Math.Sin(c), 2) + Math.Tan(c));
+        double p4 = p2 / p3;
+
+        Console.WriteLine(p4);
+        Console.ReadKey();
+    }
+}
