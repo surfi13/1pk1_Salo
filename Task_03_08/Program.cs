e89d6a61bab8e2275331869068a22b7d894c9553
Артем Сало <artemsalo15@gmail.com>
2025-01-23 22:57:52 +0500

    сделал 8 задание

---
+++ Task_03_08/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_03_08
+{
+
+    /*Даны натуральные числа от 20 до 50. Напечатать те которые делятся на 3 но не делятся на 5*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for(int i = 20; i <= 50; i++)
+            {
+                if (i % 3 == 0 && i % 5 != 0)
+                    Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task_03_09/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_03_09
+{
+
+    /*Вклад в бвнке составляет х рублей. Ежегодно он увеличивается на р процентов, после чего
+     дробнвя часть копеек отбрасывется. Каждый год сумма вклада становится больше. Определите, через
+    сколько лет не менее у рублей*/
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите начальный вклад");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите прочентеую ставку");
+            double p = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите желаемую сумму");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            int j = 0;
+            for (double i = 0; ; i++)
+            {
+                x = x * ((100 + p) / 100);
+                j++;
+                if (y <= x)
+                    break;
+             
+            }
+
+            Console.WriteLine("Желаемая сумма накопится через " + j + " лет");
+            Console.ReadKey();
+        }
+    }
+}
