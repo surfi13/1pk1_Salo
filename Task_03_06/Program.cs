70df90abb72d6c86f32f652e3b1fb9cb0a92b2c5
Артем Сало <artemsalo15@gmail.com>
2025-01-23 21:56:18 +0500

    6

--- Task_03_06/Program.cs
+++ Task_03_06/Program.cs
@@ -7,15 +7,21 @@ using System.Threading.Tasks;
 namespace Task_03_06
 {
 
-    /*Написать программу которая выводит значение функции с шаго 0.5*/
+    /*Написать программу которая выводит значение функции с шагом 0.5*/
 
     class Program
     {
         static void Main(string[] args)
         {
 
-
-
+            double y;
+            Console.WriteLine("Х равно  У равно");
+            for(double x = -4;x <= 4; x += 0.5)
+            {
+                y = Math.Abs(x);
+                Console.WriteLine(x + "       " + y);
+            }
+            Console.ReadKey();
         }
     }
 }
