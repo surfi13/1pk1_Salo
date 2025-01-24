ac9068031485ab4d72c221dad9d5f2e569a98c08
Артем Сало <artemsalo15@gmail.com>
2025-01-25 03:43:31 +0500

    jhv

---
+++ Task_04_09/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_04_09
+{
+
+    //Найти в массиве элементы которые встречвются только один раз
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rdn = new Random();
+            int[] mas = new int[50];
+
+            for (int i = 0; i < 50; i++)
+            {
+                mas[i] = rdn.Next(0, 30);
+
+                
+            }
+            
+        }
+    }
+}
---
+++ Task_04_10/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_04_10
+{
+
+    /*Заполнить массив из 10 элементов числами в диапозоне от -10 до 10 и сделать ревёрс 
+     отдульно для ккаждой из двух половин*/
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           Random rdn = new Random();
+            int[] mas = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                mas[i] = rdn.Next(-10, 11);
+                Console.Write(mas[i]);
+            }
+
+            for (int j = 4; j > 0; j--)
+            {
+                for (int k = 0; k < 5; k++)
+                {
+                    mas[j] = mas[k];
+                }
+            }
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
