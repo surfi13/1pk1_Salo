86388b7f1e701ca806683f0599255248bae79b92
Артем Сало <artemsalo15@gmail.com>
2025-01-28 02:27:09 +0500

    Слелал 4 задание

--- Task_03_04/Program.cs
+++ Task_03_04/Program.cs
@@ -6,10 +6,29 @@ using System.Threading.Tasks;
 
 namespace Task_03_04
 {
+
+    /*Пользователь вводит с консоли произвольный текст. После каждого ввода консоль очищается. Когда 
+    пользователь вводит слово "ext" или пустую строку-ввод останавливается и выводится кол-во срок введённым им*/
+
     class Program
     {
         static void Main(string[] args)
         {
+            string srt;
+            int i = 0;
+
+            while (true) {
+                Console.WriteLine("Введите любой текст");
+                srt = Console.ReadLine();
+                Console.Clear();
+                if (srt == "ext" || srt == "")
+                    break;
+                i++;
+            }
+            Console.WriteLine("Вы ввели " + i + " строк");
+            Console.ReadKey();
+
+
         }
     }
 }
