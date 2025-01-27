311f5905139c5e9adfa78fbfac6c943590d8101e
Артем Сало <artemsalo15@gmail.com>
2025-01-28 02:59:08 +0500

    Условия к задачам

---
+++ Task_05_07/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_05_07
+{
+    /*у пользователя запрпшивается число н и генерруютсч квадратный массив целых чисел
+     в лиапозоне от 10 до 99. Найти и вывести отдельно минимальный элемент матрицы 
+    Осущевствит умножение матрицы на него и при выводе помечить цвето 5 макс знчений*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Task_05_09/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_05_09
+{/*Дан кватратный массив Произведите анализ матрицы и выясните являеся ли она Z-матрицей
+  Если дпнное уссловие выплняется то вывести её с цветовой индикацией главной диагонали
+    Если нет то вывести сообщение о то что она таковой не является*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
