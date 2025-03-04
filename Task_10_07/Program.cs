4d6e8d1eaba9c4696623a7fe5397945e5fee77fd
HametShest <allaterraindustries@gmail.com>
2025-03-05 02:10:58 +0500

    Сделал 8 задание

---
+++ Task_10_07/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_10_07
+{
+    /*Cоздайте метод с помощью которого можно сгенерировать и вернуть символьный двухмерный
+    массив(состоящий из символов русского алфавита) и выведите данный массив с помощью
+    другого метода(который принимает данные массива в качестве параметра*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] array = Mass(2, 2);
+            PrintArray(array);
+            Console.ReadKey();
+        }
+
+        public static char Mass(int a, int b)
+        {
+            char[,] mass = new char[a, b];
+            Random rnd = new Random();
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    mass[i, j] = (char)rnd.Next('А', 'Я');
+                }
+            }
+
+            return mass[a,b];
+        }
+
+        public static void PrintArray(char[,] mass)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write(mass[i, j] + " ");
+                }
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
---
+++ Task_10_08/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_10_08
+{ 
+    /*Создайте метод который на вход принимает одномерный массив и число для поиска,
+     верните индекс искомого элемента в массиве. Если элемента нет - верните индекс = -1*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int[] mass = new int[100];
+            Random rnd = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                mass[i] = rnd.Next(1, 100);
+                Console.Write(mass[i] + " ");
+            }
+            ArrayPoisk(mass, N);
+            Console.ReadKey();
+        }
+
+        public static void ArrayPoisk(int[] mas, int n)
+        {
+            int j = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                if (mas[i] == n)
+                {
+                    Console.WriteLine("\n" + i);
+                    j++;
+                    break;
+                }
+            }
+            if (j == 0)
+                Console.WriteLine("\n-1");
+        }
+    }
+}
