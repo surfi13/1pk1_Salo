c959ac3bec40397fce487d3d99cb04a0a79825b1
HametShest <allaterraindustries@gmail.com>
2025-03-05 02:41:13 +0500

    Сделал 2 задание

--- Task_11_02/Program.cs
+++ Task_11_02/Program.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace Task_11_02
 {
+    /*Передача по ссылке Напишите метод который принримает два целых числа и меняет их местами*/
     class Program
     {
         static void Main(string[] args)
         {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Lakir(ref a, ref b);
+            Console.WriteLine(a + " " + b);
+            Console.ReadKey();
+        }
+
+        public static void Lakir(ref int a, ref int b)
+        {
+            int c = a;
+            a = b;
+            b = c;
+            Console.WriteLine(a + " " + b);
         }
     }
 }
