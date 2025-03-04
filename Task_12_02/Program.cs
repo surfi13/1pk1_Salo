69096723e92fcde87bef66a25481a2b2ada773c7
HametShest <allaterraindustries@gmail.com>
2025-03-05 03:17:01 +0500

    Сделал 2 задание

---
+++ Task_12_02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_12_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat_Dog dog = new Cat_Dog();
+            dog.animal = "Собака";
+            dog.name = "Дружок";
+            dog.years = 6;
+            dog.Info();
+
+            Cat_Dog cat = new Cat_Dog();
+            cat.animal = "Кошка";
+            cat.name = "Белка";
+            cat.years = 12;
+            cat.Info();
+
+            Console.ReadKey();
+        }
+    }
+}
