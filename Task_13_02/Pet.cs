8a2b619ead32c3ec7f6846e0acf80453b563cd4c
HametShest <allaterraindustries@gmail.com>
2025-03-05 08:34:45 +0500

    Сделал 2 задание

---
+++ Task_13_02/Pet.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_13_02
+{
+    class Pet
+    {
+        public string name;
+        public string animal;
+        public int height;
+        public int years;
+        public bool heath;
+
+        public void Info()
+        {
+            Console.WriteLine("Имя питомца " + name);
+            Console.WriteLine("Вид питомца" + animal);
+            Console.WriteLine("Вес питомца" + height);
+            Console.WriteLine("Возраст питомца" + years);
+            Console.WriteLine("Здоров: " + heath);
+        }
+        public void Heath(bool hp)
+        {
+            heath = hp;
+        }
+        public void Height(int hg)
+        {
+            height = hg;
+        }
+        
+        public Pet() { }
+        public Pet(string na, string amal, int heig, int year, bool heth)
+        {
+            name = na;
+            animal = amal;
+            height = heig;
+            years = year;
+            heath = heth;
+        }
+        public Pet(string na, int heig, int year, bool heth)
+        {
+            name = na;
+            height = heig;
+            years = year;
+            heath = heth;
+        }
+        public Pet(string amal, int year, bool heth)
+        {
+            animal = amal;
+            years = year;
+            heath = heth;
+        }
+        public Pet(string na, string amal, int heig, int year)
+        {
+            name = na;
+            animal = amal;
+            height = heig;
+            years = year;
+        }
+        public Pet(int heig, bool heth)
+        {
+            height = heig;
+            heath = heth;
+        }
+    }
+}
---
+++ Task_13_02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_13_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pet pet = new Pet(35, false);
+            pet.Info();
+            pet.Height(27);
+            pet.Heath(true);
+            pet.Info();
+            Console.ReadKey();
+        }
+    }
+}
