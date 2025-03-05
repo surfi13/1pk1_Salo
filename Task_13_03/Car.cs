c202da6a437c8d38b2f79e6d7aec2a53ba297559
HametShest <allaterraindustries@gmail.com>
2025-03-05 08:15:35 +0500

    Cделал 3 задание

---
+++ Task_13_03/Car.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_13_03
+{
+    class Car
+    {
+        public string number;
+        public string marka;
+        public string color;
+        public float speed;
+
+        public void Drive()
+        {
+            if (speed < 230)
+            {
+                for (int i = 1; i < 461; i++)
+                {
+                    speed ++;
+                    if (speed >= 230)
+                    {
+                        for (int j = 1; j < 231;j++)
+                        {
+                            speed --;
+                            if (speed <= 0)
+                            {
+                                Console.WriteLine("Машина остановилась");
+                                break;
+                            }
+                            Console.WriteLine(speed);
+                        }    
+                    }
+                    Console.WriteLine(speed);
+                }
+            }
+        }
+
+        public Car(string num, string mar, string col, float sp)
+        {
+            number = num;
+            marka = mar;
+            color = col;
+            speed = sp;
+        }
+        public Car(string num, string mar, float sp)
+        {
+            number = num;
+            marka = mar;
+            speed = sp;
+        }
+        public Car(float sp)
+        {
+            speed = sp;
+        }
+        public Car(string mar, float sp)
+        {
+            marka = mar;
+            speed = sp;
+        }
+        public Car() { }
+    }
+}
---
+++ Task_13_03/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_13_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car { speed = 55f };
+            car.Drive();
+            Console.ReadKey();
+        }
+    }
+}
