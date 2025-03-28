a6f56f8a91b15f20846f67a280fe6d840343c875
HametShest <allaterraindustries@gmail.com>
2025-03-28 14:54:37 +0500

    Сделал 3 задание

---
+++ Task_19_03/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_19_03
+{
+    /*Напишите программму в котором осущевстляется по строчный ввод
+     ввод завершается после ввода пустой строки. После чего вывемти 
+    их все используля разделитель*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст");
+            string str = Console.ReadLine();
+            string strn = "";
+            while (str != "")
+            {
+               strn += str;
+               str = Console.ReadLine();
+               strn += "-";
+            }
+            int i = strn.LastIndexOf("-");
+            Console.WriteLine(strn.Substring(0, i - 1));
+            Console.ReadKey();
+        }
+    }
+}
